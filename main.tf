35de9415a0d61ef4ae24fde1c69e3342c434dbd9
venkymca <venkychepuri132@gmail.com>
2020-12-12 18:53:35 +0530

    instances and sgroup

--- main.tf
+++ main.tf
@@ -65,4 +65,46 @@ resource "aws_route_table_association" "terraform-public" {
     route_table_id = "${aws_route_table.terraform-public.id}"
 }
 
+// sequrity group
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = "${aws_vpc.default.id}"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    }
+}
+
+
+// create instances:
+
+resource "aws_instance" "EC2-Instance" {
+    count= 2
+     ami = "${lookup(var.amis,var.aws_region)}" 
+     availability_zone = "${element(var.azs, count.index)}"
+     instance_type = "t2.micro"
+     key_name = "VenkyRadha"
+     subnet_id = "${element(aws_subnet.subnets.*.id, count.index)}"
+     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+     associate_public_ip_address = true	
+     tags = {
+         Name = "Server-${count.index+1}"
+         Env = "Prod"
+         Owner = "Sree"
+ 	
+     }
+ }
+
 
