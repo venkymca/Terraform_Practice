a1ff3098b660656d836b1a0c0ee6a0f4bb6eb62e
venkymca <venkychepuri132@gmail.com>
2020-12-12 18:54:17 +0530

    amis and instance types

--- variables.tf
+++ variables.tf
@@ -18,3 +18,22 @@ description = "CIdrs block for the subnets"
 type = "list"
 default = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24","10.1.4.0/24","10.1.5.0/24"]
 }
+
+variable "amis" {
+    description = "AMIs by region"
+    default = {
+        us-east-1 = "ami-97785bed" # ubuntu 14.04 LTS
+		us-east-2 = "ami-f63b1193" # ubuntu 14.04 LTS
+		us-west-1 = "ami-824c4ee2" # ubuntu 14.04 LTS
+		us-west-2 = "ami-f2d3638a" # ubuntu 14.04 LTS
+    }
+}
+
+variable "instance_type" {
+  type = "map"
+  default = {
+    dev = "t2.nano"
+    test = "t2.micro"
+    prod = "t2.medium"
+    }
+}
